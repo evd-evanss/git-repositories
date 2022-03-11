0d4d44c4ba496e21d5bf0aa00841838d344e06eb
Evandro Costa <revandro77@yahoo.com.br>
2022-03-10 23:25:01 -0300

    Created pipeline for execute tests in pr

--- app/src/test/java/com/sugarspoon/repositoriosgit/ui/github/GitHubViewModelTest.kt
+++ app/src/test/java/com/sugarspoon/repositoriosgit/ui/github/GitHubViewModelTest.kt
@@ -58,13 +58,41 @@ class GitHubViewModelTest : BaseViewModelTest() {
     }
 
     @Test
+    fun `should persist screen state when app is rotated`() = runBlockingTest {
+        //given
+        val pageCount = 1
+        val fakeDataPersisted = mutableListOf<RepositoryEntity>()
+        fakeDataPersisted.add(
+            RepositoryEntity(
+                repositoryName = "fakeRepo",
+                forks = 5,
+                stars = 5,
+                avatar = "fakeAvatar",
+                author = "fakeAuthor",
+                url = "fakeUrl"
+            )
+        )
+        coEvery { repository.getRepositories(pageCount) } returns flowOf(fakeDataPersisted)
+
+        //when
+        viewModel.dataLocal.addAll(fakeDataPersisted)
+        viewModel.handle(GitHubIntent.InitData)
+
+        //then
+        state emittedOnce GitHubState.UpdateData(fakeDataPersisted)
+    }
+
+    @Test
     fun `should load repositories, update pageCount, display shimmer, hide shimmer when I open the app`() = runBlockingTest {
+        //given
         val pageCount = 1
         val expected = mockk<List<RepositoryEntity>>()
         coEvery { repository.getRepositories(pageCount) } returns flowOf(expected)
 
+        //when
         viewModel.handle(GitHubIntent.LoadRepositories)
 
+        //then
         state emittedOnce GitHubState.UpdateCounter(page = pageCount)
         state emittedOnce GitHubState.DisplayShimmer(isLoading = true)
         state emittedOnce GitHubState.UpdateData(expected)
@@ -73,12 +101,15 @@ class GitHubViewModelTest : BaseViewModelTest() {
 
     @Test
     fun `should load repositories, when I swipe down the screen`() = runBlockingTest {
+        //given
         val pageCount = 1
         val expected = mockk<List<RepositoryEntity>>()
         coEvery { repository.getRepositories(pageCount) } returns flowOf(expected)
 
+        //when
         viewModel.handle(GitHubIntent.OnScreenSwipeDown)
 
+        //then
         state emittedOnce GitHubState.UpdateCounter(page = pageCount)
         state emittedOnce GitHubState.DisplayShimmer(isLoading = true)
         state emittedOnce GitHubState.UpdateData(expected)
@@ -87,12 +118,15 @@ class GitHubViewModelTest : BaseViewModelTest() {
 
     @Test
     fun `should load repositories when click in try again button`() = runBlockingTest {
+        //given
         val counterSaved = 5
         val expected = mockk<List<RepositoryEntity>>()
         coEvery { repository.getRepositories(counterSaved) } returns flowOf(expected)
 
+        //when
         viewModel.handle(GitHubIntent.TryAgain(counterSaved))
 
+        //then
         state emittedOnce GitHubState.DisplayShimmer(isLoading = true)
         state emittedOnce GitHubState.UpdateData(expected)
         state emittedOnce GitHubState.DisplayShimmer(isLoading = false)
@@ -100,10 +134,13 @@ class GitHubViewModelTest : BaseViewModelTest() {
 
     @Test
     fun `should open details when click in item repository`() {
+        //given
         val repositoryEntity = mockk<RepositoryEntity>()
 
+        //when
         viewModel.handle(GitHubIntent.OpenDetails(repositoryEntity))
 
+        //then
         state emittedOnce GitHubState.OpenDetails(repositoryEntity)
     }
 }
